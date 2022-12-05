9d845a9d695aa88973234aa9a2d5e4522b6fd41a
ahmedhossam <hoss4614@gmail.com>
2022-12-05 19:39:18 +0200

    finish style page

--- script.js
+++ script.js
@@ -10,17 +10,15 @@ function book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
 const addBook = document.querySelector('.add-book');
 const overlay = document.querySelector('.overlay');
 const addBookModal = document.getElementById('addBookModal');
 const body = document.querySelector('.body');
 const cardsContainer = document.querySelector('.cards-container');
 const remove = document.querySelector('.remove');
+const submit = document.getElementById('submit');
 
+// Book Items
 const bookCard = document.querySelectorAll('.book-card');
 const bookName = document.querySelector('.book-name');
 const author = document.querySelector('.book-author');
@@ -41,6 +39,46 @@ function closeAddBookModal() {
   }
 }
 
+function getBookFromInput() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  return new book(title, author, pages);
+}
+
+function addBookToLibrary() {
+  const newBook = getBookFromInput();
+
+  // create new book
+  const bookCard = document.createElement('div');
+  const name = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const readBtn = document.createElement('button');
+  const removeBtn = document.createElement('button');
+
+  bookCard.classList.add('book-card');
+  name.className.add('book-name');
+  author.className.add('book-author');
+  pages.className.add('book-pages');
+  readBtn.classList.add('is-read');
+  removeBtn.classList.add('remove');
+
+  title.textContent = `"${newBook.title}"`;
+  author.textContent = newBook.author;
+  pages.textContent = `${newBook.pages} pages`;
+  removeBtn.textContent = 'Remove';
+
+  bookCard.appendChild('name');
+  bookCard.appendChild('author');
+  bookCard.appendChild('pages');
+  bookCard.appendChild('readBtn');
+  bookCard.appendChild('removeBtn');
+  cardsContainer.appendChild(bookCard);
+
+  closeAddBookModal();
+}
+
 function removeCard() {
   const bookCard = document.querySelectorAll('.book-card');
   bookCard.remove();
@@ -49,3 +87,6 @@ function removeCard() {
 addBook.addEventListener('click', openAddBookModal);
 overlay.addEventListener('click', closeAddBookModal);
 remove.addEventListener('click', removeCard);
+submit.addEventListener('click', addBookToLibrary);
+
+console.log(myLibrary);
